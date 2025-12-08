f872e5136d83b6dee2079bcb6fe80878938841b0
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-08 10:22:47 +0300

    header and footer part of the NFL

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Home from './page/Home/Home'
+import './App.css'
+import './index.css'
 
 const App = () => {
   return (
-    <div>App</div>
+   <>
+   <Home />
+   </>
   )
 }
 
