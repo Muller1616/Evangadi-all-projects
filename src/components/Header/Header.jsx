281ac84bfc67eee97632a6b1a1c345ace8e4d3ca
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-10 15:14:50 +0300

    Row page and responsive all pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,49 @@
-import React from 'react'
-import './header.css'
-import netLogo from '../../assets/images/netflixlogo.png'
+import React, { useState } from 'react';
+import './header.css';
+import netLogo from '../../assets/images/netflixlogo.png';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Header = () => {
-  return (
-    <div className='header_outer_container'>
-        <div className="header_container">
-            <div className="header_left">
-                <ul>
-                    <li><img src={netLogo} alt="Netflix Logo" width = '100' /></li>
-                    <li>Home</li>
-                    <li>TVShows</li>
-                    <li>Movies</li>
-                    <li>Latest</li>
-                    <li>MyList</li>
-                    <li>Browse by Languages</li>
-                </ul>
-            </div>
-            <div className="header_right">
-                <ul>
-                    <li><SearchIcon /> </li>
-                    <li> <NotificationsNoneIcon /> </li>
-                    <li><AccountBoxIcon /></li>
-                    <li><ArrowDropDownIcon /></li>
-                </ul>
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+    return (
+        <div className='header_outer_container'>
+            <div className="header_container">
+                <div className="header_left">
+                    <ul className={isOpen ? 'nav_active' : ''}>
+                        <li><img src={netLogo} alt="Netflix Logo" width='100' /></li>
+                        <li>Home</li>
+                        <li>TV Shows</li>
+                        <li>Movies</li>
+                        <li>Latest</li>
+                        <li>My List</li>
+                        <li>Browse by Languages</li>
+                    </ul>
+                </div>
+
+                <div className="header_right">
+                    <ul className={isOpen ? 'nav_active' : ''}>
+                        <li><SearchIcon /></li>
+                        <li><NotificationsNoneIcon /></li>
+                        <li><AccountBoxIcon /></li>
+                        <li><ArrowDropDownIcon /></li>
+                    </ul>
+
+                    {/* Hamburger icon */}
+                    <div className="hamburger" onClick={toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Header
+export default Header;
